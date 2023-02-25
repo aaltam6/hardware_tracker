270ed245b80df07fd3ddd7c6f22a7076fa8dc6d0
aaltam6 <87050733+aaltam6@users.noreply.github.com>
2023-02-24 19:38:36 -0600

    Add files via upload

--- hardware_tracker.sql
+++ hardware_tracker.sql
@@ -2,16 +2,29 @@ USE sys;
 DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS loc;
 DROP TABLE IF EXISTS hardware;
+DROP TABLE IF EXISTS rent;
 SET FOREIGN_KEY_CHECKS=0;
 
 CREATE TABLE employee(
-rentID varchar(50),
 employeeID varchar(50),
 firstName varchar(50),
 lastName varchar(50),
 positionTitle varchar(50),
 department varchar(50),
-PRIMARY KEY(rentID)
+PRIMARY KEY(employeeID)
+)
+;
+CREATE TABLE rent(
+rentID varchar(50),
+checkOut varchar(50),
+checkIn varchar(50),
+employeeID varchar(50),
+hardwareID varchar(50),
+locID varchar(50),
+PRIMARY KEY(rentID),
+FOREIGN KEY(employeeID) REFERENCES employee(employeeID),
+FOREIGN KEY(hardwareID) REFERENCES hardware(hardwareID),
+FOREIGN KEY(locID) REFERENCES loc(locID)
 )
 ;
 CREATE TABLE loc(
@@ -29,33 +42,31 @@ hardwareID varchar(50),
 itemType varchar(50),
 make varchar(50),
 model varchar(50),
-checkOut date,
-checkedInStatus varchar(50),
-activeStatus varchar(10),
 locID varchar(50),
 rentID varchar(50),
 PRIMARY KEY(hardwareID),
 FOREIGN KEY (locID) REFERENCES loc(locID),
-FOREIGN KEY (rentID) REFERENCES employee(rentID)
+FOREIGN KEY (rentID) REFERENCES rent(rentID)
 )
 ;
 
-INSERT INTO employee(rentID,employeeID,firstName,lastName,positionTitle,department)
-VALUES('R001','E001','Arturo','Altamirano','Loan Officer','Lending'),
-('R002','E002','Clyde','Drexler','Accountant','Accounting'),
-('R003','E003','Allen','Iverson','CEO','Executive'),
-('R004','E003','Allen','Iverson','CEO','Executive');
+INSERT INTO employee(employeeID,firstName,lastName,positionTitle,department)
+VALUES('E001','Arturo','Altamirano','IT Intern','Network Team'),
+('E002','Clyde','Drexler','Junior Accountant','Accounting')
+;
 
-INSERT INTO hardware(hardwareID,itemType,make,model,checkOut,checkedInStatus,activeStatus,locID,rentID)
-VALUES('PC050','Personal Computer','Dell','GB1100','2021-05-12','Checked Out','Active','L001','R001'),
-('M009','Monitor','Dell','DM-991','2021-05-12','Checked In','Inactive','L001','R002'),
-('PC182','Personal Computer','Intel','RZ-200','2020-01-01','Checked Out','Active','L002','R003'),
-('LT001','Laptop','Dell','Lattitude E550','2021-02-02','Checked Out','Active','L002','R004');
+INSERT INTO rent(rentID,checkOut,checkIn,employeeID,hardwareID,locID)
+VALUES('R001','2021-01-01','NULL','E001','PC050','L001'),
+('R002','2019-09-28','2022-01-01','E002','M009','L002')
+;
+
+INSERT INTO hardware(hardwareID,itemType,make,model,locID,rentID)
+VALUES('PC050','Personal Computer','Dell','GB1100','L001','R001'),
+('M009','Monitor','Dell','DM-991','L002','R002')
+;
 
 INSERT INTO loc(locID,officeTitle,city,State,officeType,employeeCount)
 VALUES('L001','Corporate HeadQuarters','Baton Rouge','LA','Corporate Multi-Floor','200'),
-('L002','Single Space','Mobile','AL','Single Floor','10'),
-('L003','Auxiliary Corporate Office','Slidell','LA','Corporate Single-Floor','126'),
-('L004','Auxiliary Corporate Office','New Orleans','LA','Corporate Single-Floor','189')
+('L002','Single Space','Mobile','AL','Single Floor','10')
 ;
 
